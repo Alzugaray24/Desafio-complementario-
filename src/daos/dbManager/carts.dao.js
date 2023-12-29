f63879b6cdd2bfbc9b725602a600a2cc8cebd418
Matias Alzugaray <alzugaray1997@gmail.com>
2023-12-29 19:21:48 -0300

    actualizando los carritos

--- src/daos/dbManager/carts.dao.js
+++ src/daos/dbManager/carts.dao.js
@@ -1,29 +1,57 @@
-import { cartsModel } from "../../models/carts.model.js"
+// CartsDao.js
+
+import { cartsModel } from "../../models/carts.model.js";
 
 class CartsDao {
-    constructor() {
-        this.model = cartsModel;
-    }
+  constructor() {
+    this.model = cartsModel;
+  }
 
-    async getAllCarts() {
-        return await this.model.find().lean();
-    }
+  async getAllCarts() {
+    return await this.model.find().lean();
+  }
 
-    async getCartById(cartId) {
-        return await this.model.findById(cartId).lean();
-    }
+  async getCartById(cartId) {
+    return await this.model.findById(cartId).populate("products.product").lean();
+  }
 
-    async addCart(cart) {
-        return await this.model.create(cart);
-    }
+  async addCart(cart) {
+    return await this.model.create(cart);
+  }
 
-    async updateCart(cartId, updatedCart) {
-        return await this.model.findByIdAndUpdate(cartId, updatedCart, { new: true });
-    }
+  async updateCart(cartId, updatedCart) {
+    return await this.model.findByIdAndUpdate(cartId, updatedCart, { new: true }).populate("products.product");
+  }
 
-    async deleteCart(cartId) {
-        return await this.model.findByIdAndDelete(cartId);
-    }
+  async deleteCart(cartId) {
+    return await this.model.findByIdAndDelete(cartId);
+  }
+
+  async updateProductQuantity(cartId, productId, quantity) {
+    const updatedCart = await this.model.findOneAndUpdate(
+      { _id: cartId, "products.product": productId },
+      { $set: { "products.$.quantity": quantity } },
+      { new: true }
+    ).populate("products.product");
+
+    return updatedCart;
+  }
+
+  async clearCart(cartId) {
+    const clearedCart = await this.model.findByIdAndUpdate(cartId, { products: [] }, { new: true }).populate("products.product");
+
+    return clearedCart;
+  }
+
+  async removeProductFromCart(cartId, productId) {
+    const updatedCart = await this.model.findByIdAndUpdate(
+      cartId,
+      { $pull: { products: { product: productId } } },
+      { new: true }
+    ).populate("products.product");
+
+    return updatedCart;
+  }
 }
 
 export default CartsDao;
--- src/routes/carts.routes.js
+++ src/routes/carts.routes.js
@@ -5,60 +5,119 @@ const cartsRouter = Router();
 const carts = new CartsDao();
 
 cartsRouter.get("/", async (req, res) => {
-    try {
-        const mostrarCarts = await carts.getAllCarts();
-        res.render("carts", { mostrarCarts });
-    } catch (error) {
-        console.error("Error al obtener todos los carritos:", error);
-        res.status(500).json({ error: "Internal Server Error" });
-    }
+  try {
+    const mostrarCarts = await carts.getAllCarts();
+    res.render("carts", { mostrarCarts });
+  } catch (error) {
+    console.error("Error al obtener todos los carritos:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+cartsRouter.get("/:cid", async (req, res) => {
+  try {
+    const cartId = req.params.cid;
+
+    const cart = await carts.getCartById(cartId);
+
+    res.status(200).json({
+      status: "success",
+      cart,
+    });
+  } catch (error) {
+    console.error("Error al obtener el carrito:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 cartsRouter.post("/", async (req, res) => {
-    try {
-        const cart = req.body;
-        const nuevoCart = await carts.addCart(cart);
-  
-        res.status(201).json({
-            status: "success",
-            cart: nuevoCart,
-        });
-    } catch (error) {
-        console.error("Error al agregar el carrito", error);
-        res.status(500).json({ error: "Internal Server Error" });
-    }
+  try {
+    const cart = req.body;
+    const nuevoCart = await carts.addCart(cart);
+
+    res.status(201).json({
+      status: "success",
+      cart: nuevoCart,
+    });
+  } catch (error) {
+    console.error("Error al agregar el carrito", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
-cartsRouter.put("/:id", async (req, res) => {
-    try {
-        const cartId = req.params.id;
-        const updatedCart = req.body;
+cartsRouter.put("/:cid", async (req, res) => {
+  try {
+    const cartId = req.params.cid;
+    const updatedCart = req.body;
 
-        const updatedCartResult = await carts.updateCart(cartId, updatedCart);
+    const updatedCartResult = await carts.updateCart(cartId, updatedCart);
 
-        res.status(200).json({
-            status: "success",
-            cart: updatedCartResult,
-        });
-    } catch (error) {
-        console.error("Error al actualizar el carrito", error);
-        res.status(500).json({ error: "Internal Server Error" });
-    }
+    res.status(200).json({
+      status: "success",
+      cart: updatedCartResult,
+    });
+  } catch (error) {
+    console.error("Error al actualizar el carrito", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
-cartsRouter.delete("/:id", async (req, res) => {
-    try {
-        const cartId = req.params.id;
-        const deletedCart = await carts.deleteCart(cartId);
+cartsRouter.put("/:cid/products/:pid", async (req, res) => {
+  try {
+    const cartId = req.params.cid;
+    const productId = req.params.pid;
+    const { quantity } = req.body;
 
-        res.status(200).json({
-            status: "success",
-            cart: deletedCart,
-        });
-    } catch (error) {
-        console.error("Error al eliminar el carrito", error);
-        res.status(500).json({ error: "Internal Server Error" });
-    }
+    const updatedCart = await carts.updateProductQuantity(
+      cartId,
+      productId,
+      quantity
+    );
+
+    res.status(200).json({
+      status: "success",
+      cart: updatedCart,
+    });
+  } catch (error) {
+    console.error(
+      "Error al actualizar la cantidad del producto en el carrito",
+      error
+    );
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+cartsRouter.delete("/:cid", async (req, res) => {
+  try {
+    const cartId = req.params.cid;
+
+    const clearedCart = await carts.clearCart(cartId);
+
+    res.status(200).json({
+      status: "success",
+      cart: clearedCart,
+    });
+  } catch (error) {
+    console.error("Error al eliminar todos los productos del carrito", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+cartsRouter.delete("/:cid/products/:pid", async (req, res) => {
+  try {
+    const cartId = req.params.cid;
+    const productId = req.params.pid;
+
+    const updatedCart = await carts.removeProductFromCart(cartId, productId);
+
+    res.status(200).json({
+      status: "success",
+      cart: updatedCart,
+    });
+  } catch (error) {
+    console.error("Error al eliminar el producto del carrito", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 export default cartsRouter;
